83aed430efa9e2287415b8f179efb18a28b00692
Samuel Ohiri <therealsammy18x@gmail.com>
2022-12-07 08:24:43 +0100

    Add gitignore

--- fundme/FundMe.sol
+++ fundme/FundMe.sol
@@ -2,11 +2,6 @@
 pragma solidity ^0.8.0;
 import "./PriceConverter.sol";
 
-// GOALS:
-// Get funds from users
-// Withdraw funds
-// Set a minimum funding value in USD
-
 contract FundMe {
     using PriceConverter for uint256;
 
@@ -17,13 +12,10 @@ contract FundMe {
 
 
     function fund() public payable {
-        // Want to be able to set a minimum fund amount in USD
-        // 1. How do we send ETH to this contract?
 
-        require (msg.value.getConversionRate() >= minimumUsd, "Didn't send enough!"); // 1e18 == 1 * 10 ** 18 == 1,000,000,000,000,000,000
+        require (msg.value.getConversionRate() >= minimumUsd, "Didn't send enough!");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = msg.value;
-        
     }
 
 
