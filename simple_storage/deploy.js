dafcb88a320e2a4fb9627b1d2d869135d60e118c
Samuel Ohiri <therealsammy18x@gmail.com>
2022-12-07 20:23:22 +0100

    Binary file and abi tested.... Working.

--- simple_storage/deploy.js
+++ simple_storage/deploy.js
@@ -12,9 +12,9 @@ async function main() {
   wallet = await wallet.connect(provider);
   //const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 
-  const abi = fs.readFileSync("./_SimpleStorage_sol_SimpleStorage.abi", "utf8");
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
   const binary = fs.readFileSync(
-    "./_SimpleStorage_sol_SimpleStorage.bin",
+    "./SimpleStorage_sol_SimpleStorage.bin",
     "utf8"
   );
   const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
